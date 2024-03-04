3da793a66c980edf446dd57688d5187d9639cb23
JongHan Jeon <moohan211@naver.com>
2024-03-04 18:30:20 +0900

    feat: 스프링 MVC - 시작하기 실습 코드 추가
    Member 가입, 저장, 조회 Controller 추가 - Spring MVC

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @(@Controller)
+ * 		스프링이 자동으로 스프링 빈으로 등록한다. (내부에 @Component 애노테이션이 있어서 컴포넌트 스캔의 대상이 됨)
+ * 		스프링 MVC에서 애노테이션 기반 컨트롤러로 인식한다.
+ *
+ * @(@RequestMapping)
+ * 		요청 정보를 매핑한다.
+ * 		해당 URL이 호출되면 이 메서드가 호출된다.
+ * 		애노테이션을 기반으로 동작하기 때문에, 메서드의 이름은 임의로 지으면 된다.
+ *
+ * @(ModelAndView)
+ * 		모델과 뷰 정보를 담아서 반환하면 된다.
+ */
+@Controller
+// @Component
+// @RequestMapping
+// 스프링 부트 3.0부터는 @Controller 가 있어야 스프링이 컨트롤러로 인식하는 것으로 바뀌었음.
+public class SpringMemberFormControllerV1 {
+
+	@RequestMapping("/springmvc/v1/members/new-form")
+	public ModelAndView process() {
+		return new ModelAndView("new-form");
+	}
+}
